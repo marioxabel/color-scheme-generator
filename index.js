e3285b881fb86abe93c913b3788519d65153506f
marioxabel <31260739+marioxabel@users.noreply.github.com>
2022-11-03 22:05:22 -0600

    logic completed

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 document.getElementById("get-color").addEventListener("submit", function(e) {
     e.preventDefault()
-    const baseColor = document.getElementById("base").value
+    // Added the slice to get rid of the "#" in the hex baseColor
+    const baseColor = document.getElementById("base").value.slice(1,7)
     const mode = document.getElementById("mode").value
-    const data = {
-        base: baseColor,
-        mode: mode
-    }
-    console.log(data)
-    fetch("https://www.thecolorapi.com/scheme?hex=ff0000&mode=complement&count=6 ")
-        .then(response => response.json())
-        .then(res => console.log(res))
+    // Test log
+    console.log(mode)
+
+    
+    fetch("https://www.thecolorapi.com/scheme?hex=" + baseColor + "&mode=" + mode + "&count=6 ")
+        .then(response => res = response.json())
+        .then(data => {
+            // Save colors from api
+            const color1 = data.colors[0].hex.value
+            const color2 = data.colors[1].hex.value
+            const color3 = data.colors[2].hex.value
+            const color4 = data.colors[3].hex.value
+
+            // color 0 is the baseColor
+            document.getElementById("color-0").style.background  = "#" + baseColor;
+            document.getElementById("color-1").style.background  = color1;
+            document.getElementById("color-2").style.background  = color2;
+            document.getElementById("color-3").style.background  = color3;
+            document.getElementById("color-4").style.background  = color4;
+
+        })
+       
+    
+        
+
 })
