9fd7566f339c2a2d3161aa0c773ccb9ceda78aca
marioxabel <31260739+marioxabel@users.noreply.github.com>
2022-11-03 22:29:21 -0600

    names added

--- index.js
+++ index.js
@@ -16,13 +16,19 @@ document.getElementById("get-color").addEventListener("submit", function(e) {
             const color3 = data.colors[2].hex.value
             const color4 = data.colors[3].hex.value
 
-            // color 0 is the baseColor
+            // Color the divs, color 0 is the baseColor
             document.getElementById("color-0").style.background  = "#" + baseColor;
             document.getElementById("color-1").style.background  = color1;
             document.getElementById("color-2").style.background  = color2;
             document.getElementById("color-3").style.background  = color3;
             document.getElementById("color-4").style.background  = color4;
 
+            //Set the names 
+            document.getElementById("name-0").innerHTML  = "#" + baseColor;
+            document.getElementById("name-1").innerHTML  = color1;
+            document.getElementById("name-2").innerHTML  = color2;
+            document.getElementById("name-3").innerHTML  = color3;
+            document.getElementById("name-4").innerHTML  = color4;
         })
        
     
